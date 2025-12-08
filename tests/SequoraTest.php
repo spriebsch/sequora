7f7de5ff1d984e7c973cbf2cb57c8c54e19abfca
Stefan Priebsch <stefan@priebsch.de>
2025-12-08 13:23:16 +0100

    Tests

--- tests/SequoraTest.php
+++ tests/SequoraTest.php
@@ -48,4 +48,37 @@ final class SequoraTest extends TestCase
         $this->assertInstanceOf(TestEvent::class, $result[2]);
         $this->assertSame('three', $result[2]->payload);
     }
+
+    public function test_query(): void
+    {
+        $connection = SqliteConnection::memory();
+
+        $schema = SqliteSequoraSchema::from($connection);
+        $schema->createIfNotExists();
+
+        $writer = SequoraWriter::from(SqliteDatabaseWriter::from($connection));
+        $reader = SequoraReader::from(SqliteDatabaseReader::from($connection, require __DIR__ . '/doubles/TopicMap.php'));
+
+        $events = [
+            new TestEvent('one'),
+            new TestEvent('two'),
+            new TestEvent('three'),
+        ];
+
+        $writer->store(...$events);
+
+        $query = EventQuery::from();
+
+        $result = $reader->query($query)->asArray();
+
+        $this->assertCount(3, $result);
+        $this->assertInstanceOf(TestEvent::class, $result[0]);
+        $this->assertSame('one', $result[0]->payload);
+
+        $this->assertInstanceOf(TestEvent::class, $result[1]);
+        $this->assertSame('two', $result[1]->payload);
+
+        $this->assertInstanceOf(TestEvent::class, $result[2]);
+        $this->assertSame('three', $result[2]->payload);
+    }
 }
