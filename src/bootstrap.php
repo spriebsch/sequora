b58f722badcc13bbcff2ae44e81a17dc8993b65b
Stefan Priebsch <stefan@priebsch.de>
2026-01-19 01:12:22 +0100

    100% Coverage

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,9 @@
 <?php declare(strict_types=1);
 
+// @codeCoverageIgnoreStart
 require __DIR__ . '/autoload.php';
 
 if (is_file(__DIR__ . '/../vendor/autoload.php')) {
     require __DIR__ . '/../vendor/autoload.php';
 }
+// @codeCoverageIgnoreEnd
