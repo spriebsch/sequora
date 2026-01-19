392fe0230d78709eaee4ee74d69d189660db58ba
Stefan Priebsch <stefan@priebsch.de>
2026-01-19 08:48:44 +0100

    WIP

--- tests/EventQueryTest.php
+++ tests/EventQueryTest.php
@@ -89,7 +89,7 @@ final class EventQueryTest extends TestCase
         $this->assertEquals($causationId, $query->causationIdValue());
     }
 
-    public function test_starting_after_sets_and_returns_event_id(): void
+    public function test_after_eventId(): void
     {
         $eventId = EventId::generate();
 
@@ -104,27 +104,6 @@ final class EventQueryTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Unknown condition key(s): invalid');
 
-        EventQuery::from(['invalid' => 'x']);
-    }
-
-    public function test_after_event_id_key_is_allowed_in_constructor(): void
-    {
-        $eventId = EventId::generate();
-
-        $query = EventQuery::from(['afterEventId' => $eventId]);
-
-        $this->assertNotNull($query->afterValue());
-        $this->assertTrue($eventId->equals($query->afterValue()));
-    }
-
-    public function test_with_topics_and_starting_after_both_preserved(): void
-    {
-        $eventId = EventId::generate();
-        $topic = Topic::fromString('the-vendor.the-domain.the-context.the-name');
-
-        $query = EventQuery::from()->withTopics($topic)->after($eventId);
-
-        $this->assertSame([$topic], $query->topicsValue());
-        $this->assertTrue($eventId->equals($query->afterValue()));
+        EventQuery::from(['invalid' => 'the-value']);
     }
 }
