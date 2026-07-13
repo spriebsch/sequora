16f17696b1fd252ffebae17005aadcac2beaec3b
Stefan Priebsch <stefan@priebsch.de>
2026-07-13 10:03:24 +0200

    Cleanup bootstrapping

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,9 +1,3 @@
 <?php declare(strict_types=1);
 
-// @codeCoverageIgnoreStart
-require __DIR__ . '/autoload.php';
-
-if (is_file(__DIR__ . '/../vendor/autoload.php')) {
-    require __DIR__ . '/../vendor/autoload.php';
-}
-// @codeCoverageIgnoreEnd
+require __DIR__ . '/../vendor/autoload.php';
